7e7e05f1c926ab0c4dcbc867cc1058c775f5310f
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-25 11:08:54 +0300

    Adding welcoming page

--- awardapp/views.py
+++ awardapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    date = dt.date.today()
+    return render(request,'welcome.html')
